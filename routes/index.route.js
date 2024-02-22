8fa24235e02c894a11e330d93ee8d82cc3de0fb2
AlbertoPavarin <albertopavarin@libero.it>
2024-02-22 16:07:10 +0100

    import validateToken

--- routes/index.route.js
+++ routes/index.route.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const validateToken = require('./../middleware/validateToken');
 
 router.use("/auth", require("./auth/auth.route"));
+
+router.all('*', validateToken);
 
 module.exports = router;
