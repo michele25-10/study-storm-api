2542b31f42a7a0dc1696e128487f88cebb33d92a
michele25-10 <michele.gabri01@gmail.com>
2024-03-05 12:30:00 +0100

    API resetPassword aggiunti controlli, da testare e da sistemare il corpo della mail

---
+++ models/reset-password.js
@@ -0,0 +1,25 @@
+const moment = require('moment/moment');
+const connFunction = require('../utils/executeMySql');
+
+const TABLE = "reset_password";
+
+const ResetPassword = {
+    insertResetPassword: async ({ idu }) => {
+        const result = await connFunction.insert(TABLE, {
+            id_user: idu,
+            date_created: moment().format('YYYY-MM-DD HH:mm:ss')
+        })
+        return result;
+    },
+    checkResetPassword: async ({ idu }) => {
+        const mysql = `
+        select rp.id
+        from reset_password rp 
+        inner join user u on u.id = rp.id_user
+        where rp.date_created >= NOW() - INTERVAL 1 DAY and rp.idu like @idu and rp.verifaied = '0'`;
+        const result = await connFunction.query(mysql, { idu });
+        return result;
+    }
+}
+
+module.exports = ResetPassword;
--- routes/auth/auth.controller.js
+++ routes/auth/auth.controller.js
@@ -1,6 +1,7 @@
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
 const User = require('../../models/user.model');
+const ResetPassword = require('../../models/reset-password');
 const { hash } = require('../../utils/crypto');
 const sendMailer = require('../../utils/mail');
 
@@ -53,21 +54,21 @@ const registration = asyncHandler(async (req, res) => {
     const hashedPassword = hash(req.body.password);
     req.body.password = hashedPassword;
 
-    let result = await User.selectUserByEmail({email: req.body.email});
+    let result = await User.selectUserByEmail({ email: req.body.email });
 
-    if (result.length > 0){ // utente già registrato
+    if (result.length > 0) { // utente già registrato
         res.status(400);
         throw new Error();
     }
 
-    result = await  User.insertVerification({user_credentials: JSON.stringify(req.body)});
+    result = await User.insertVerification({ user_credentials: JSON.stringify(req.body) });
 
     if (result.affectedRows != 1) {
         res.status(400);
         throw new Error();
     }
 
-    result = await User.retrieveVerification({id: result.insertId});
+    result = await User.retrieveVerification({ id: result.insertId });
 
     if (result.length != 1) {
         res.status(404);
@@ -109,10 +110,20 @@ const registration = asyncHandler(async (req, res) => {
 //@route POST /api/auth/forgot-password
 //@access public
 const forgotPassword = asyncHandler(async (req, res) => {
+    //Controllo se la mail esiste nel db del progetto
     const mailExists = await User.selectUserByEmail({ email: req.body.email, alsoDisactive: false });
-    if (mailExists.length === 0) {
+    if (mailExists.length != 1) {
         res.status(404);
         throw new Error("Email inesistente");
+    }
+
+    const idu = mailExists[0].id;
+
+    // Controllo se è già stata inviata una mail di password dimenticata nelle ultime 24 ore ancora attiva
+    const existResetPassword = await ResetPassword.checkResetPassword({ idu });
+    if (existResetPassword.length > 0) {
+        res.status(404);
+        throw new Error('La mail è già stata inviata');
     }
 
     await sendMailer({
@@ -122,26 +133,51 @@ const forgotPassword = asyncHandler(async (req, res) => {
         text: "",
         html: `
         <!DOCTYPE html>
-            <html lang="en">
+            <html lang="it">
             <head>
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <title>Email con Bottone Invia</title>
+                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
             </head>
             <body>
-                <h1>Password Dimenticata!</h1>
-                <p>Gentile utente, qualora avessi dimenticato la password premi il bottone conferma!</br>
-                Qualora invece non fossi stato tu a richiedere il cambio password, assicurati che la tua password sia sicura:</br>
-                ${req.headers['user-agent']}</p>
+            <script type="text/javascript">
+                async function confirm() {
+                    fetch("http://localhost:5010/api/reset-password/confirm")
+                    .then((res) => {
+                        console.log("mail inviata"); 
+                        alert("Mail con nuova password inviata");
+                    }).catch((err) => {
+                        console.log("Errore"); 
+                        alert("Errore"); 
+                    }); 
+                }
+            </script>
+
+            <h1>Password Dimenticata!</h1>
+
+                <p class="container mt-2">Gentile utente, qualora avessi dimenticato la password premi il bottone conferma!<br></p>
+
+                <p class="container mt-2">Qualora invece non fossi stato tu a richiedere il cambio password, assicurati che la tua password sia sicura:<br>
+                <i>${req.headers['user-agent']}</i></p>
                 
+                <p class="container mt-2">Se non sei tu che stai cercando di recuperare la tua password inviaci una mail all'indirizzo:<br>
+                studentime@gmail.com</p>
+
                 <!-- Bottone Invia -->
-                <form action="http://localhost:5010/reset-password/confirm" method="post">
-                    <button type="submit">Confermo!</button>
-                </form>
+                <button class="btn btn-success" onclick="confirm()">Confermo!</button>
+
+                <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
             </body>
             </html>
         `
     });
+
+    const result = await ResetPassword.insertResetPassword({ idu });
+    if (result.affectedRows != 1) {
+        res.status(500);
+        throw new Error();
+    }
 
     res.status(200).send({ message: "Controlla le tue mail!" });
 });
