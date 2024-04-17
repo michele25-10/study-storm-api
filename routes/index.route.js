ee4c3e52cd93ba817b54ade8304945b1c0d62f25
michele25-10 <michele.gabri01@gmail.com>
2024-04-17 08:41:10 +0200

    Rotta student-type non ha bisongo di token

--- routes/index.route.js
+++ routes/index.route.js
@@ -7,11 +7,11 @@ router.use("/reset-password", require("./reset-password/reset-password.route"));
 router.use("/invite-team", require("./invite-team/invite-team.route"));
 router.use("/newsletter", require("./newsletter/newsletter.route"));
 router.use("/img-profile", require("./img-profile/img-profile.route"));
+router.use("/student-type", require("./student-type/student-type.route"));
 
 //Rotte con token
 router.all('*', validateToken);
 router.use("/user", require("./user/user.route"));
-router.use("/student-type", require("./student-type/student-type.route"));
 router.use("/goal", require("./goal/goal.route"));
 router.use("/question", require("./question/question.route"));
 router.use("/user-goal", require("./user-goal/user-goal.route"));
