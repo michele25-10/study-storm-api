2ef1ad2460265aca3f06af78274463bdf34f0870
michele25-10 <michele.gabri01@gmail.com>
2024-06-26 08:58:30 +0200

    API Stats nuove

--- models/stats.model.js
+++ models/stats.model.js
@@ -44,6 +44,54 @@ const Stats = {
 
         return result;
     },
+    selectLastSevenTotStats: async ({ idu, numberOfDays }) => {
+        let mysql = "";
+
+        mysql = `
+        SELECT g.id, g.name, ROUND(SUM(a.minutes) / 60) AS hour, pc.primary_color, pc.secondary_color
+        FROM user_task_agenda uta
+        INNER JOIN agenda a ON a.id = uta.id_agenda
+        INNER JOIN task t ON t.id = uta.id_task
+        INNER JOIN goal g ON g.id = t.id_goal
+        INNER JOIN palette_color pc ON pc.id = g.id_palette
+        WHERE uta.id_user = @idu
+            AND (a.date BETWEEN DATE_SUB(CURDATE(), INTERVAL @numberOfDays DAY) AND CURDATE())
+        GROUP BY g.id;`;
+
+        const result = await connFunction.query(mysql, {
+            idu, numberOfDays
+        });
+
+        return result;
+    },
+    selectInfoStudy: async ({ idu, numberOfDays }) => {
+        let mysql = "";
+
+        mysql = `
+        SELECT g.id, g.name, ROUND(AVG(a.minutes) / 60) AS avg, count(a.id) AS frequency, 
+        (
+           SELECT ROUND(AVG(a2.minutes) / 60)
+           FROM user_task_agenda uta2
+           INNER JOIN agenda a2 ON a2.id = uta2.id_agenda
+           INNER JOIN task t2 ON t2.id = uta2.id_task
+           WHERE uta2.id_user = @idu
+             AND (a2.date BETWEEN DATE_SUB(CURDATE(), INTERVAL ${numberOfDays * 2} DAY) 
+                            AND DATE_SUB(CURDATE(), INTERVAL @numberOfDays DAY))
+           GROUP BY uta2.id_user
+       ) AS last_avg
+        FROM user_task_agenda uta
+        INNER JOIN agenda a ON a.id = uta.id_agenda
+        INNER JOIN task t ON t.id = uta.id_task
+        WHERE uta.id_user = @idu
+            AND (a.date BETWEEN DATE_SUB(CURDATE(), INTERVAL @numberOfDays DAY) AND CURDATE())
+        GROUP BY uta.id_user;`;
+
+        const result = await connFunction.query(mysql, {
+            idu, numberOfDays
+        });
+
+        return result;
+    },
 };
 
 module.exports = Stats;
--- routes/stats/stats.route.js
+++ routes/stats/stats.route.js
@@ -5,5 +5,9 @@ const statsController = require('./stats.controller');
 const statsValidation = require('./stats.validation');
 const validate = require('../../middleware/JoiValidation');
 
+router.get("/study/seven-days/", statsController.getLastSevenDaysStats);
+router.get("/study/thirty-days/", statsController.getLastThirtyDaysStats);
+router.get("/study/info/", statsController.getStudyInfo);
 router.get("/study/period/", validate(statsValidation.getStatsHourStudy), statsController.getStatsHourStudy);
+
 module.exports = router;
